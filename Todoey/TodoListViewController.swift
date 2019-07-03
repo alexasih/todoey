b0feb3f516fa355eac48c4244e2cdd192d78878c
Alexa Sih <alexa17cassidy@gmail.com>
2019-07-03 16:02:45 -0700

    add selection feature to tableview

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -17,6 +17,8 @@ class TodoListViewController: UITableViewController {
    
     }
     
+    //MARK: - Tableview Datasource Methods
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
@@ -28,6 +30,22 @@ class TodoListViewController: UITableViewController {
         
         return cell
     }
+    
+    //MARK: - Tableview Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+            
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
 
 
 }
